f06c6b69621260d07446d8907e65656ced268049
Hemal-Brahmbhatt <hemal.brahmbhatt@brainvire.com>
2022-04-22 19:53:24 +0530

    Update 2

--- app/src/main/java/com/brain/courses_app/di/CourseApp.kt
+++ app/src/main/java/com/brain/courses_app/di/CourseApp.kt
@@ -1,7 +1,14 @@
 package com.brain.courses_app.di
 
 import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CourseApp:Application()
+class CourseApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseAnalytics.getInstance(this)
+    }
+
+}
